d7f9a132c50bb7ff0cc3bad72ccd77a325a6f1cf
DidBer <didrikber@gmail.com>
2024-11-12 21:13:11 +0100

    Added Player Detection and Moving Toward Player

--- Assets/Virus.cs
+++ Assets/Virus.cs
@@ -1,13 +1,17 @@
+using JetBrains.Annotations;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Virus : MonoBehaviour
 {
     public Rigidbody2D virusBody;
+    public BoxCollider2D detectionRange;
     public bool playerDetected = true;
     public float moveSpeed;
     private float timer = 10;
     private float cycle = 0;
     private float cycle2 = 10;
+    private float player_x_value;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -39,6 +43,19 @@ public class Virus : MonoBehaviour
                 cycle = 0;
             }
         }
+        else
+        {
+            VirusMoveStop(); 
+            float virus_x_value = virusBody.transform.position.x;
+            if (virus_x_value < player_x_value)
+            {
+                VirusMoveRight();
+            }
+            else
+            {
+                VirusMoveLeft();
+            }
+        }
     }
     void VirusMoveRight()
     {
@@ -54,4 +71,29 @@ public class Virus : MonoBehaviour
     {
         virusBody.linearVelocity = Vector2.zero;
     }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerDetected = false;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerDetected = true;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player_x_value = collision.transform.position.x;
+        } 
+    }
+
 }
