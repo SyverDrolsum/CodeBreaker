6eb7f986e9c4166c1f6a3acf105ac946a09a7da5
SyverDrolsum <syver.drolsum@gmail.com>
2024-11-21 14:10:28 -0300

    Added recharge station
    Recharge station adds health to player, then disappears. Disappears also when on full helath

---
+++ Assets/Prefabs/RechargeStation/Recharge Station Behaviour.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class RechargeStationBehaviour : MonoBehaviour
+{
+    public Health health;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            health.Heal(1);
+
+        }
+    }
+}
