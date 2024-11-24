cb99edfcbd15b052e4805b7739ae3f29ad577e62
AlphaLizzard <idalarsen993@gmail.com>
2024-11-24 18:47:00 +0100

    clean up

--- Assets/Prefabs/PauseMenu/PauseMenu.cs
+++ Assets/Prefabs/PauseMenu/PauseMenu.cs
@@ -2,42 +2,45 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    public GameObject pauseMenu;
+    public GameObject pauseMenu;        //reference to pause menu GameObject
 
     void Start()
     {
-        Cursor.visible = false;
+        Cursor.visible = false;     //hides cursor when the game starts
     }
 
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Escape))
+
+        if (Input.GetKeyUp(KeyCode.Escape))     //activate or deactivate pause menu
         {
-            if (!pauseMenu.activeSelf)
+            if (!pauseMenu.activeSelf)      //activate if not activated
             {
-                Time.timeScale = 0f;
-                pauseMenu.SetActive(true);
-                Cursor.visible = true;
+                Time.timeScale = 0f;        //freeze game   
+                pauseMenu.SetActive(true);      //show pause menu
+                Cursor.visible = true;      //make cursor visible
             }
 
             else
             {
-                Time.timeScale = 1f;
-                pauseMenu.SetActive(false);
-                Cursor.visible = false;
+                Time.timeScale = 1f;        //resume game
+                pauseMenu.SetActive(false);     //hide pause menu
+                Cursor.visible = false;     //hide cursor
             }
         }
     }
 
+    //function to quit application
     public void quit()
     {
-        Application.Quit();
+        Application.Quit();     //close game
     }
 
+    //function to resume game
     public void resume()
     {
-        Time.timeScale = 1f;
-        pauseMenu.SetActive(false);
-        Cursor.visible = false;
+        Time.timeScale = 1f;        //resume game
+        pauseMenu.SetActive(false);     //hide pause menu   
+        Cursor.visible = false;     //hide cursor
     }
 }
