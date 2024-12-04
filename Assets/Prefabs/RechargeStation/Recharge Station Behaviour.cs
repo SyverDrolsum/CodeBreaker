13f91823f424d689fb4f8b2aca633724d65f6769
DidBer <didrikber@gmail.com>
2024-12-04 15:43:01 +0100

    Fix

--- Assets/Prefabs/RechargeStation/Recharge Station Behaviour.cs
+++ Assets/Prefabs/RechargeStation/Recharge Station Behaviour.cs
@@ -4,13 +4,17 @@ public class RechargeStationBehaviour : MonoBehaviour
 {
     public Health health;
     [SerializeField] private AudioClip rechargeSound;
+    private GameObject targetObject;
+
   
     //When Player touches rechargeStation, the station disappears and heals player
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            SoundManager.instance.PlaySound(rechargeSound);
+            targetObject = GameObject.FindGameObjectWithTag("Player");
+            health = targetObject.GetComponent<Health>();
+//            SoundManager.instance.PlaySound(rechargeSound);
             Destroy(gameObject);
             health.Heal(1);
 
